eada74fe45adaf54cbc9222ed49d4b931410adc0
naari3 <naari.named@gmail.com>
2021-07-05 04:43:37 +0900

    Add wav

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use async_process::Command;
 use async_std::{
     fs::{copy, create_dir_all},
     io::{self, prelude::WriteExt},
     path::Path,
 };
 use indicatif::ProgressBar;
+use regex::Regex;
 use std::env;
 
 use anyhow::Result;
@@ -79,6 +81,69 @@ async fn main() -> Result<()> {
         pb
     });
     pb.finish_with_message("done");
+
+    // TODO: refactor
+    println!("awb -> wav");
+    let mut tasks = vec![];
+    create_dir_all(dest.join("wav")).await?;
+
+    println!("collect");
+    let epb = ProgressBar::new(tasks.len() as u64);
+    for entry in glob::glob("./dest/**/*.awb").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => {
+                let output = Command::new(".\\vgmstream-win\\test.exe")
+                    .args(&["-m", path.to_str().unwrap()])
+                    .output()
+                    .await?;
+                let metainfo_string = String::from_utf8_lossy(&output.stdout).to_string();
+                let re = Regex::new(r"stream count: (\d+)")?;
+                let caps = match re.captures(&metainfo_string) {
+                    Some(caps) => caps,
+                    None => continue,
+                };
+                let stream_count: u32 = caps.get(1).unwrap().as_str().parse()?;
+                create_dir_all(dest.join("wav").join(path.clone().parent().unwrap())).await?;
+                for i in 1..stream_count + 1 {
+                    let path = path.clone();
+                    tasks.push(async_std::task::spawn(async move {
+                        let status = Command::new(".\\vgmstream-win\\test.exe")
+                            .args(&[
+                                "-s",
+                                &i.to_string(),
+                                path.clone().to_str().unwrap(),
+                                "-o",
+                                &format!(
+                                    "dest/wav/{}/?n_?s.wav",
+                                    path.clone().parent().unwrap().to_string_lossy()
+                                ),
+                            ])
+                            .status()
+                            .await
+                            .unwrap();
+                        if !status.success() {
+                            println!("failed {:?}", path);
+                        }
+                    }));
+                }
+            }
+            Err(e) => println!("{:?}", e),
+        }
+        epb.inc(1);
+    }
+    epb.finish_with_message("done");
+
+    println!("found {} resources", tasks.len());
+    let vgm_pb = ProgressBar::new(tasks.len() as u64);
+
+    let vgm_pb = task::block_on(async move {
+        for task in tasks.into_iter() {
+            task.await;
+            vgm_pb.inc(1);
+        }
+        vgm_pb
+    });
+    vgm_pb.finish_with_message("done");
 
     Ok(())
 }
