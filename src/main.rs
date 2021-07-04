70e8bcb17e2400847318fd87244403c4e94a3554
naari3 <naari.named@gmail.com>
2021-07-04 21:37:02 +0900

    Impl sample

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
+use std::env;
+
 use anyhow::Result;
+use rusqlite::Connection;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Asset {
+    name: String,
+    hash: String,
+}
 
 fn main() -> Result<()> {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let sql_path = args.get(1).expect("Please specify meta file path.");
+    println!("{}", sql_path);
+    let conn = Connection::open(sql_path)?;
+
+    let mut stmt = conn.prepare("SELECT n, h FROM a LIMIT 5;")?;
+    let asset_iter = stmt.query_map([], |r| {
+        Ok(Asset {
+            name: r.get(0)?,
+            hash: r.get(1)?,
+        })
+    })?;
+
+    for asset in asset_iter {
+        println!("{:?}", asset.unwrap());
+    }
 
     Ok(())
 }
